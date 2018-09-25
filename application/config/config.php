05c2084737a6b553f77e1930c096c84408305deb
supriya2me <supriya2saha94@gmail.com>
2018-09-25 19:49:33 +0530

    last update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.1.1/koushik/med_note/';
+$config['base_url'] = 'http://staging.mednotemr.com/';
 
 /*
 |--------------------------------------------------------------------------
